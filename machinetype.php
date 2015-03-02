4590983a299e1c53d5cba553f4962a2655199ebd
Paul Laibach <github@laibach.net>
2015-03-02 08:24:38 -0500

    lookup devices
    base version v1.0 of lookup devices applet

---
+++ machinetype.php
@@ -0,0 +1,53 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="machinetype.css">
+<title>Desktop Device Lookup v1.0</title>
+
+</head>
+
+<body>
+
+
+<p>
+Desktop Device Lookup v1.0<br>
+Furry search of Desktop_Name, Marketing_Name, and Machine_Type
+
+<form name="lookupForm" action="machinetype.php" method ="POST">
+
+
+<input type="text" autofocus placeholder="gimme something" onFocus="this.select()" name="model" value="<?php if (isset($_POST['model'])) echo $_POST['model']; ?>">
+
+<p><input type="submit" class="submit"></p> 
+		
+		</form>
+</p>
+<p>
+<?php
+# Connect to MySQL server and the database
+require( 'includes/connect_db.php' ) ;
+
+# Includes these helper functions
+require( 'includes/machineHelpers.php' ) ;
+
+	if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
+
+		$model = $_POST['model'];
+
+		new_machine_search($dbc, $model);
+		}
+	else if($_SERVER[ 'REQUEST_METHOD' ] == 'GET') 
+{
+	if(isset($_GET['model']))
+	{
+find_record_machine($dbc, $_GET['model']) ;	
+	}	
+}
+
+mysqli_close( $dbc ) ;
+?>
+</p>		
+</body>
+</html>
