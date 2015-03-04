2a83a55e3aefdb32cc7d9287e7a126940381d243
Paul Laibach <github@laibach.net>
2015-03-04 09:49:20 -0500

    Concurrent_v2_Development
    Files to use when working with new format

---
+++ LcMS_applets_v2/lookup_devices.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Life-cycle Management Applet Collection
+     Paul Laibach - Project Lead
+     Connor Hayes - Research & Development
+-->
+
+<head>
+   <title>LcMS Device Lookup v2.0</title>
+   <link rel="stylesheet" type="text/css" href="lookup_applets.css">
+</head>
+
+<body>
+   <div class="wrapper">
+   LcMS Device Lookup v2.0<br>
+   Furry search of Life-cycle Management System Devices Table<br>
+   <br>
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pellentesque quis arcu sed mollis. Ut efficitur lobortis iaculis. Vestibulum a mi eu arcu eleifend sollicitudin. Vivamus porta auctor ligula at egestas. Nulla consectetur, dui in condimentum pellentesque, neque purus aliquam odio, ut tristique ipsum neque sit amet turpis. Integer commodo erat id turpis ornare molestie. Donec quis cursus quam. Vestibulum pharetra risus quis velit laoreet tincidunt. Donec vel risus non ante placerat faucibus et ac tellus. Maecenas id fermentum quam. In et nibh vitae tellus commodo pharetra in vitae dui. Nunc placerat, augue ac facilisis sollicitudin, neque lectus gravida nisi, sed aliquet ante ligula in felis. Etiam vitae elit lorem. Nam vulputate, lorem in sagittis dapibus, urna orci tincidunt erat, eu laoreet purus ex sit amet eros.<br>
+   <br>
+   Morbi consequat libero nisi, id pulvinar mi gravida vitae. Curabitur in venenatis libero. Suspendisse a tempus eros. Ut vitae metus in leo iaculis pharetra. Nunc facilisis magna vel sapien tristique tempus vitae auctor ligula. Suspendisse potenti. Nulla eget congue magna, vel viverra est. Maecenas sed sodales nisl. Vestibulum eget leo quis lorem auctor rutrum. Etiam ut molestie ligula. In hac habitasse platea dictumst. Sed molestie volutpat augue, at ullamcorper libero sagittis non.<br>
+   </div>
+</body>
+
+</html>
